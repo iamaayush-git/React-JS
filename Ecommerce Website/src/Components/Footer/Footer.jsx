64b5ffe9239b49256009758ca4702baea2d1b50a
iamaayush-git <aayushbhattaraia6@gmail.com>
2024-04-11 21:03:19 +0545

    Project Updated

---
+++ Ecommerce Website/src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <>
+      <h2>This is footer</h2>
+    </>
+  );
+}
+export default Footer;
--- Ecommerce Website/src/Components/NewsLetter/NewsLetter.jsx
+++ Ecommerce Website/src/Components/NewsLetter/NewsLetter.jsx
@@ -1,9 +1,16 @@
-import "./NewsLetter.css"
-function NewsLetter(){
-    return(
-        <div className="main">
-            <p>This is newsLetter div for tomorrow</p>
+import "./NewsLetter.css";
+function NewsLetter() {
+  return (
+    <div className="main">
+      <div className="new_letter">
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe Our NewLetter and Stay Updated</p>
+        <div className="input_field">
+          <input type="email" placeholder="Your Email Id" />
+          <button>Subscribe</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default NewsLetter
+export default NewsLetter;

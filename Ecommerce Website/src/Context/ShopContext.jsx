520884eb1feab7a708f0534792f7dc03c613f83e
iamaayush-git <aayushbhattaraia6@gmail.com>
2024-04-17 22:27:56 +0545

    project updated

--- Ecommerce Website/src/Context/ShopContext.jsx
+++ Ecommerce Website/src/Context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import { createContext } from "react";
 import all_product from "../Components/assets/all_product"
+
 export const shopContext = createContext(null);
 
 const ShopContextProvider = (props)=>{
-  const contextValue = {all_product};
-
+  const contextValue = all_product;
   return(
   <shopContext.Provider value={contextValue}>
     {props.children}
--- Ecommerce Website/src/Pages/ShopCatagory.jsx
+++ Ecommerce Website/src/Pages/ShopCatagory.jsx
@@ -1,13 +1,28 @@
 import { useContext } from "react";
 import { shopContext } from "../Context/ShopContext";
-function ShopCatagory(props){
-    const {all_product} = useContext(shopContext);
-    console.log(all_product);
-    return(
-        <>
-       
-        
-        </>
-    )
-}   
+import Item from "../Components/Item/Item";
+import "./CSS/ShopCatagory.css"
+function ShopCatagory(props) {
+  const data = useContext(shopContext);
+  console.log(props.category);
+  return (
+    <div className="shop_catagory">
+      <img src={props.banner} alt="" />
+      <div className="shop_item">
+      {data.map((item, i) => {
+        if (props.category == item.category) {
+          return (
+            <Item
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        }
+      })}
+      </div>
+    </div>
+  );
+}
 export default ShopCatagory;

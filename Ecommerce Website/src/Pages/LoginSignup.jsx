8bdcf7c78126f127b1174cd025488469720c029d
iamaayush-git <aayushbhattaraia6@gmail.com>
2024-05-07 22:01:13 +0545

    created LoginSignup page

--- Ecommerce Website/src/Pages/LoginSignup.jsx
+++ Ecommerce Website/src/Pages/LoginSignup.jsx
@@ -1,8 +1,25 @@
-function LoginSignup(){
-    return(
-        <>
-        <h2>login</h2>
-        </>
-    )
+import './CSS/LoginSignup.css';
+
+function LoginSignup() {
+  return (
+    <>
+      <div className="loginSignupMain">
+        <h2 className="signup">Sign Up</h2>
+        <div className="inputField">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button className="continue_btn">Continue</button>
+        <footer>
+          <p>
+            Already have an account? <span>Login here</span>
+          </p>
+          <input type="checkbox" id='check'/> <label htmlFor="check">By continuing, I agree to the terms of use &
+          privacy policy. </label>
+        </footer>
+      </div>
+    </>
+  );
 }
 export default LoginSignup;

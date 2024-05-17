c94c3e57172f557274a6934df757098afe763d99
iamaayush-git <aayushbhattaraia6@gmail.com>
2024-05-17 14:51:42 +0545

    file updated

--- Ecommerce Website/src/Components/ProductDisplay/ProductDisplay.jsx
+++ Ecommerce Website/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,60 @@
 import "./ProductDisplay.css";
+import star_icon from "../assets/star_icon.png";
+import star_dull from "../assets/star_dull_icon.png";
 function ProductDisplay({ product }) {
+  console.log(product);
   return (
     <div className="productDisplay">
-      <div className="productdisplay_left"></div>
-      <div className="productdisplay_right"></div>
+      <div className="productdisplay_left">
+        <div className="productdisplayimage_list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay_img">
+          <img src={product.image} alt="" className="productdisplaymain_img" />
+        </div>
+      </div>
+      <div className="productdisplay_right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay_right_star">
+          <div className="starImages">
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_dull} alt="" />
+            <img src={star_dull} alt="" />
+          </div>
+          <p>(122)</p>
+        </div>
+        <div className="productdisplayright_prices">
+          <div className="productdisplayright_oldprice">
+            ${product.old_price}
+          </div>
+          <div className="productdisplayright_newprice">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplayright_size">
+          <h2>Select Size</h2>
+          <div className="product_sizes">
+          <div>S</div>
+          <div>M</div>
+          <div>L</div>
+          <div>XL</div>
+          <div>XXL</div>
+          </div>
+        </div>  
+        <button>Add To Cart</button>
+        <div className="productdisplayright_category">
+          <span>Catagory : </span>
+          <span>Women, T-Shirt, Crop Top</span>
+        </div>
+        <div className="productdisplayright_category">
+          <span>Tags : </span>
+          <span>Modern, Latest</span>
+        </div>
+      </div>
     </div>
   );
 }
